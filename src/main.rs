a8b734640f9c22ec9578cc1394e5d4b1d08bf56e
Andrei Stepanenko <ftvkyo@gmail.com>
2024-03-12 14:18:19 +0000

    Configure ClearColor

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ use bevy::{
     sprite::{MaterialMesh2dBundle, Mesh2dHandle},
 };
 
+const COLOR_BG: Color = Color::rgb(0.2, 0.8, 0.2);
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(COLOR_BG))
         .add_systems(Startup, setup)
         .run();
 }
@@ -17,12 +20,11 @@ fn setup(
 ) {
     commands.spawn(Camera2dBundle::default());
 
-    let background = Mesh2dHandle(meshes.add(Rectangle::new(10.0, 10.0)));
-
-    let color = Color::rgb(0.2, 0.8, 0.2);
+    let mesh = Mesh2dHandle(meshes.add(Rectangle::new(10.0, 10.0)));
+    let color = Color::rgb(0.8, 0.2, 0.2);
 
     commands.spawn(MaterialMesh2dBundle {
-        mesh: background,
+        mesh,
         material: materials.add(color),
         ..Default::default()
     });
