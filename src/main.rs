ff9bfbe1c11203a675ead7310bfc9fd3e3bdd556
Andrei Stepanenko <ftvkyo@gmail.com>
2024-03-17 20:23:56 +0000

    Only allow Vulkan rendering backend for wgpu

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use bevy::{
-    prelude::*,
-    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+    prelude::*, render::{settings::{Backends, WgpuSettings}, RenderPlugin}, sprite::{MaterialMesh2dBundle, Mesh2dHandle}
 };
 
 const COLOR_BG: Color = Color::rgb(0.2, 0.8, 0.2);
 
 fn main() {
+    let render_plugin = RenderPlugin {
+        render_creation: WgpuSettings {
+            backends: Some(Backends::VULKAN),
+            ..default()
+        }
+        .into(),
+        ..default()
+    };
+
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(render_plugin))
         .insert_resource(ClearColor(COLOR_BG))
         .add_systems(Startup, setup)
         .add_systems(Update, bevy::window::close_on_esc)
